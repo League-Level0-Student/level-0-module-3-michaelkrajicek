9862f854e624cc60cdda4b307dbec876d408b914
student <student@192.168.1.17>
2023-08-29 13:59:36 -0700

    hi

--- src/_01_else_if/_4_are_you_happy/Happy.java
+++ src/_01_else_if/_4_are_you_happy/Happy.java
@@ -5,14 +5,23 @@ import javax.swing.JOptionPane;
 public class Happy {
 	public static void main(String[] args) {
 		String keepOn = "Keep doing whatever you're doing.";
+		
 		String input = JOptionPane.showInputDialog("Are you Happy?");
+		
 		if (input.equalsIgnoreCase("yes")) {
+			
 			JOptionPane.showMessageDialog(null, keepOn);
+			
 		} else if (input.equalsIgnoreCase("no")) {
+			
 			input = JOptionPane.showInputDialog("Do you want to be happy");
+			
 			if (input.equalsIgnoreCase("no")) {
+				
 				JOptionPane.showMessageDialog(null, keepOn);
+				
 			} else if (input.equalsIgnoreCase("yes")) {
+				
 				JOptionPane.showMessageDialog(null, "Change something.");
 			}
 		}
